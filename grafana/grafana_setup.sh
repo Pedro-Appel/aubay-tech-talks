fd4d21c62a93c4d3c28f0462ad92598301890ea5
Ismael Miranda <ismaelmiranda11@hotmail.com>
2024-09-18 14:19:35 +0100

    new grafana dashboard

--- grafana/grafana_setup.sh
+++ grafana/grafana_setup.sh
@@ -13,8 +13,8 @@ curl -X POST http://admin:admin@localhost:3000/api/datasources \
     -H "Content-Type: application/json" \
     -d '{ "name": "prometheus", "uid":"prom-data", "type": "prometheus", "access": "proxy", "url": "http://prometheus:9090", "basicAuth": true, "isDefault":true}' \
 
-# Import dashboard
-json=$(<dash.json)
+# Import dashboards
+json=$(<basic_dash.json)
 payload="{\"dashboard\": $json, \"overwrite\": true}"
 
 curl -X POST http://admin:admin@localhost:3000/api/dashboards/import \
@@ -22,3 +22,13 @@ curl -X POST http://admin:admin@localhost:3000/api/dashboards/import \
     -H "Accept: application/json" \
     -H "Content-Type: application/json" \
     -d "$payload"
+
+json=$(<adv_dash.json)
+payload="{\"dashboard\": $json, \"overwrite\": true}"
+
+curl -X POST http://admin:admin@localhost:3000/api/dashboards/import \
+    -u "admin:admin" \
+    -H "Accept: application/json" \
+    -H "Content-Type: application/json" \
+    -d "$payload"
+    
